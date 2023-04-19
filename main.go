5cf8268b17062ff8b3f65a3889abd9ae1e0d516a
timothy Rasinski <2264381489@qq.com>
2023-04-19 21:47:34 +0800

    解决https格式错误

--- main.go
+++ main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"crypto/rand"
 	"crypto/sha1"
-	"encoding/hex"
+	"fmt"
+	"io"
 	"math/big"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"runtime/debug"
 	"sort"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/julienschmidt/httprouter"
@@ -24,34 +27,69 @@ func main() {
 	router.GET("/alert/wechat/api", ForwardHandler)
 	// router panic handler
 	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, i interface{}) {
+		glg.Errorf("stack :%s", string(debug.Stack()))
 		glg.Errorf("panic: %+v", i)
 	}
 	// HTTP Server
 	http.ListenAndServe(":8080", router)
 }
 
+type RoundTripperFunc func(*http.Request) (*http.Response, error)
+
+func (fn RoundTripperFunc) RoundTrip(request *http.Request) (*http.Response, error) {
+	return fn(request)
+}
+
 func ForwardHandler(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
 	u := &url.URL{
 		Scheme: "https",
 		Host:   "jarvis-alert.niulinkcloud.com",
-		Path:   "/alert/wechat/api",
 	}
-
+	request.URL.Path = "/alert/wechat/api?"
+	// 添加验签
+	request.ParseForm()
+	form := request.Form
 	signature, timestamp, nonce := generateSign()
-	u.Query().Set("signature", signature)
-	u.Query().Set("timestamp", timestamp)
-	u.Query().Set("nonce", nonce)
-	u.Query().Set("echostr", "test")
+	form.Add("signature", signature)
+	form.Add("timestamp", timestamp)
+	form.Add("nonce", nonce)
+	form.Add("echostr", "test")
+	body := form.Encode()
+	body = strings.TrimSpace(body)
+	unescape, err := url.QueryUnescape(body)
+	if err != nil {
+		panic(err)
+	}
+	request.Body = io.NopCloser(strings.NewReader(unescape))
+	request.ContentLength = int64(len(body))
 	proxy := httputil.NewSingleHostReverseProxy(u)
 
+	proxy.Transport = RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+		// request
+		s := req.URL.String()
+		unescape, err := url.PathUnescape(s)
+		if err != nil {
+			panic(err)
+		}
+		Url := fmt.Sprintf("%s%s", unescape, req.Form.Encode())
+		glg.Infof("Request URL: %s", Url)
+		newRequest, err := http.NewRequest(req.Method, Url, req.Body)
+		if err != nil {
+			glg.Errorf("Request Error: %+v", err)
+		}
+		cli := &http.Client{
+			Timeout: 60 * time.Second,
+		}
+		res, err := cli.Do(newRequest)
+		return res, nil
+	})
 	proxy.ServeHTTP(writer, request)
 }
 
 func InitLog() {
+	infolog := glg.FileWriter("./tmp/info.log", 0666)
 
-	infolog := glg.FileWriter("/tmp/info.log", 0666)
-
-	errlog := glg.FileWriter("/tmp/error.log", 0666)
+	errlog := glg.FileWriter("./tmp/error.log", 0666)
 	glg.Get().AddLevelWriter(glg.INFO, infolog).AddLevelWriter(glg.ERR, errlog) // add info log file destination
 
 }
@@ -68,12 +106,9 @@ func generateSign() (signature, timestamp, nonce string) {
 	var tempArray = []string{token, timestamp, nonce}
 	sort.Strings(tempArray)
 	//将三个参数字符串拼接成一个字符串进行sha1加密
-	var sha1String string = ""
-	for _, v := range tempArray {
-		sha1String += v
-	}
-	h := sha1.New()
-	h.Write([]byte(sha1String))
-	signature = hex.EncodeToString(h.Sum([]byte("")))
+	tmpArr := []string{token, timestamp, nonce}
+	sort.Strings(tmpArr)
+
+	signature = fmt.Sprintf("%x", sha1.Sum([]byte(strings.Join(tmpArr, ""))))
 	return
 }
