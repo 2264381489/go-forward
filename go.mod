0cc6d8aa4c6a356d0e1ef750c2cb0f3da1c180e1
timothy Rasinski <2264381489@qq.com>
2023-04-19 17:16:53 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module go-forward
+
+go 1.19
+
+require github.com/julienschmidt/httprouter v1.3.0
+
+require (
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/kpango/fastime v1.1.9 // indirect
+	github.com/kpango/glg v1.6.15 // indirect
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/sha1"
+	"encoding/hex"
+	"math/big"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/kpango/glg"
+)
+
+func main() {
+	router := httprouter.New()
+	InitLog()
+	// HTTP Server FORWARD  HTTP Request
+	router.GET("/v1/nodes", ForwardHandler)
+	// router panic handler
+	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, i interface{}) {
+		glg.Errorf("panic: %+v", i)
+	}
+	// HTTP Server
+	http.ListenAndServe(":8080", router)
+}
+
+func ForwardHandler(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+	u := &url.URL{
+		Scheme: "http",
+		Host:   "pcdn-env-dev-admin.qa.qiniu.io",
+		Path:   "/api/proxy/jarvis",
+	}
+	signature, timestamp, nonce := generateSign()
+	u.Query().Set("signature", signature)
+	u.Query().Set("timestamp", timestamp)
+	u.Query().Set("nonce", nonce)
+	proxy := httputil.NewSingleHostReverseProxy(u)
+	request.Host = "pcdn-env-dev-admin.qa.qiniu.io"
+
+	proxy.ServeHTTP(writer, request)
+}
+
+func InitLog() {
+
+	infolog := glg.FileWriter("/tmp/info.log", 0666)
+
+	errlog := glg.FileWriter("/tmp/error.log", 0666)
+	glg.Get().AddLevelWriter(glg.INFO, infolog).AddLevelWriter(glg.ERR, errlog) // add info log file destination
+
+}
+
+func generateSign() (signature, timestamp, nonce string) {
+	timestamp = strconv.FormatInt(time.Now().Unix(), 10)
+	token := os.Getenv("TOKEN")
+	nonce = "" // 随机数
+	for i := 0; i < 20; i++ {
+		result, _ := rand.Int(rand.Reader, big.NewInt(100))
+		nonce += strconv.FormatInt(result.Int64(), 10)
+	}
+	//将token、timestamp、nonce三个参数进行字典序排序
+	var tempArray = []string{token, timestamp, nonce}
+	sort.Strings(tempArray)
+	//将三个参数字符串拼接成一个字符串进行sha1加密
+	var sha1String string = ""
+	for _, v := range tempArray {
+		sha1String += v
+	}
+	h := sha1.New()
+	h.Write([]byte(sha1String))
+	signature = hex.EncodeToString(h.Sum([]byte("")))
+	return
+}
